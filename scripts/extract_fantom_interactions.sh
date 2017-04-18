4060f91c6b6fee9bccf19797f0adb8ab2843d00b
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-04-18 11:15:08 -0400

    Don't gzip FANTOM and Thurman interactions
    * Also add bash failsafes

--- scripts/extract_fantom_interactions.sh
+++ scripts/extract_fantom_interactions.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
+set -u
+set -o pipefail
+
 FANTOM_DATA=../data/fantom/enhancer_tss_associations.bed
 
 # Grab first three columns
 # Grab the first element of ; delimited 4th column, replace : with -, and then - with tabs
 paste <(cut -f 1-3 ${FANTOM_DATA}) <(cut -f 4 ${FANTOM_DATA} | cut -f 1 -d ';' | sed -e 's/:/-/g' | awk '{gsub("-","\t",$0); print}') > fantom.interactions
-gzip fantom.interactions
