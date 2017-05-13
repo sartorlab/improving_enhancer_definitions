60b422d5d19a3d293e7d2ac32b2e6102fd29bb74
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-05-13 11:07:20 -0400

    Last alterations for enhancer lists
    * Use GenomicRanges::setdiff() to make keep parts of enhancers that don't overlap gene loci.
    * Move create_enhancer_list() function into generate_enhancer_list.R and rename it to expand_and_resect().

--- scripts/generate_enhancer_list.R
+++ scripts/generate_enhancer_list.R
@@ -27,9 +27,53 @@ if (length(opts$extension) == 0) {
 #####  LOAD THE REST OF THE LIBRARIES  #####
 suppressPackageStartupMessages(library("GenomicRanges"))
 suppressPackageStartupMessages(library("plyr"))
-source("../scripts/create_enhancer_list.R", chdir = T)
 
+#####  FUNCTION TO EXPAND AND RESECT GRANGES  #####
+expand_and_resect <- function(granges, min_size) {
+  # extends each grange to the min_size (if not there already); if this would cause two neighboring ranges to overlap, it extends them only until they bookend
+
+  # make sure everything overlapping already has been merged
+  granges <- reduce(granges, min.gapwidth = 0)
+
+  granges$left = start(granges)
+  granges$right = end(granges)
+
+
+  # resize the ranges that are smaller than min_size; keep the ranges that are bigger than min_size as is.
+  was_resized <- rep(F, length(granges))
+  was_resized[(end(granges) - start(granges)) < min_size] <- T
+
+  granges[(end(granges) - start(granges)) < min_size] <- resize(granges[(end(granges) - start(granges)) < min_size], width = min_size, fix = "center")
+  resized_starts <- start(granges)
+  resized_ends <- end(granges)
+
+  # find the cases where, after resizing, two ranges overlap
+  overlaps <- findOverlaps(granges, granges)
+  overlaps <- overlaps[(queryHits(overlaps)+1)==subjectHits(overlaps),]
+
+
+  resect_first <- queryHits(overlaps)[was_resized[queryHits(overlaps)] & !was_resized[subjectHits(overlaps)]]
+  resect_second <- queryHits(overlaps)[!was_resized[queryHits(overlaps)] & was_resized[subjectHits(overlaps)]]
+  resect_both <- queryHits(overlaps)[was_resized[queryHits(overlaps)] & was_resized[subjectHits(overlaps)]]
+
+  # resect
+  end(granges)[resect_first] <- start(granges)[resect_first + 1]
+  start(granges)[resect_second + 1] <- end(granges)[resect_second]
+  overlap_amount <- end(granges)[resect_both] - start(granges)[resect_both+1]
+  resection_amount <- ceiling(overlap_amount / 2)
+  end(granges)[resect_both] <- end(granges)[resect_both] - resection_amount
+  start(granges)[resect_both+1] <- start(granges)[resect_both+1] + resection_amount
+
+
+  # NOTE: need to know the chromosome sizes so we can truncate at the chromosome starts/ends.  For now, just truncate at the chromosome starts...
+  start(granges)[start(granges) <= 0] <- 1
+
+  return(granges)
+}
+
+#####
 #####  MAIN  #####
+#####
 
 #####  READ GENE LOCI  #####
 genes = read.table(opts$genes, header = F, as.is = T, sep = '\t')
@@ -132,14 +176,14 @@ for(i in 1:nrow(combinations)) {
     enhancers_df = rbind(enhancers_df, fantom)
   }
 
-  enhancers_gr = makeGRangesFromDataFrame(enhancers_df)
+  enhancers_gr = makeGRangesFromDataFrame(enhancers_df, ignore.strand = TRUE)
 
-  enhancers_merged = create_enhancer_list(enhancers_gr, opts$extension)
+  # Expand enhancers, but make adjacent enhnacers within extension just bump up
+  # against each other at the midpoint
+  enhancers_merged = expand_and_resect(enhancers_gr, opts$extension)
 
-  # ditch things that overlap with genes...
-  overlaps = findOverlaps(genes_gr, enhancers_merged)
-  kick_out = unique(subjectHits(overlaps))
-  enhancers_merged = enhancers_merged[-1 * kick_out]
+  # Remove any part of genes_gr from the enhancers_gr
+  enhancers_merged = GenomicRanges::setdiff(enhancers_merged, genes_gr)
 
   enhancers_df = as.data.frame(enhancers_merged)
   enhancers_df = enhancers_df[order(enhancers_df$seqnames, enhancers_df$start), ]
@@ -147,3 +191,46 @@ for(i in 1:nrow(combinations)) {
   message(sprintf('Writing %s', out_file))
   write.table(enhancers_df[,c("seqnames", "start", "end")], file = gzfile(out_file), quote = F, sep = "\t", row.names = F, col.names = F)
 }
+
+#####  TEST expand_and_resect()  #####
+
+#df <- data.frame(chromosome=rep("chr1", 6), start = c(57, 61, 81, 97, 122, 127), end = c(60, 72, 95, 98, 124, 129))
+#df.ranges <- makeGRangesFromDataFrame(df)
+#df.ranges
+#expand_and_resect(df.ranges, min_size = 10)
+
+#for(i in seq(2, 1000)) {
+#  seqname <- paste("chr", as.character(i), sep = '')
+#  df <- data.frame(chromosome=rep(seqname, 6), start = c(57, 61, 81, 97, 122, 127), end = c(60, 72, 95, 98, 124, 129))
+#  df.ranges.new <- makeGRangesFromDataFrame(df)
+#  df.ranges <- c(df.ranges, df.ranges.new)
+#}
+#expand_and_resect(df.ranges, min_size = 10)
+
+# df <- data.frame(chromosome=rep("chr1", 4), start = c(5, 14, 20, 32), end = c(7, 17, 25, 40))
+# df.ranges <- makeGRangesFromDataFrame(df)
+# df.ranges
+# expand_and_resect(df.ranges, min_size = 1)
+#
+#
+# df <- data.frame(chromosome=rep("chr1", 4), start = c(5, 14, 20, 32), end = c(7, 17, 25, 40))
+# df.ranges <- makeGRangesFromDataFrame(df)
+# df.ranges
+# expand_and_resect(df.ranges, min_size = 5)
+#
+#
+# df <- data.frame(chromosome=rep("chr1", 4), start = c(5, 14, 20, 32), end = c(7, 17, 25, 40))
+# df.ranges <- makeGRangesFromDataFrame(df)
+# df.ranges
+# expand_and_resect(df.ranges, min_size = 2000)
+#
+#
+# df <- data.frame(chromosome=rep("chr1", 4), start = c(5, 6, 20, 32), end = c(6, 20, 25, 40))
+# df.ranges <- makeGRangesFromDataFrame(df)
+# df.ranges
+# expand_and_resect(df.ranges, min_size = 10)
+
+# df <- data.frame(chromosome=rep("chr1", 4), start = c(5, 8, 20, 32), end = c(6, 20, 25, 40))
+# df.ranges <- makeGRangesFromDataFrame(df)
+# df.ranges
+# expand_and_resect(df.ranges, min_size = 10)
