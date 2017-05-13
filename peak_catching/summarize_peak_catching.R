c98e9fb84808c1a89ef5deb5f27f5cc8914dbce5
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-05-13 17:17:20 -0400

    Update peak catching summarization

--- peak_catching/summarize_peak_catching.R
+++ peak_catching/summarize_peak_catching.R
@@ -8,6 +8,7 @@ library(ggplot2)
 # Number of peaks in the experiment
     num_peaks = read.table('num_peaks.txt', header=F, sep=' ', stringsAsFactors=F)
     colnames(num_peaks) = c('tf', 'num_peaks')
+    num_peaks$tf = gsub('.narrowPeak','',num_peaks$tf)
 
 # Number of unique peaks assigned with the locus definition
     num_uniq_peaks = read.table('num_uniq_peaks_assigned.txt', header=F, sep=' ', stringsAsFactors=F)
@@ -16,14 +17,28 @@ library(ggplot2)
 
     num_uniq_peaks = subset(num_uniq_peaks, tf != 'wgEncodePilotTfbsMyersA549GrDex')
 
+# Read in chromosomze sizes
+    chrom_sizes = read.table('~/espresso/share/genomes/hg19/chromInfo_hg19.txt', sep='\t', header=F, as.is = TRUE)
+    genome_size = sum(as.numeric(chrom_sizes[,2]))
+
+# Read in genome coverage
+    genome_coverage = read.table('genome_coverage.txt', header=F, sep = ' ', stringsAsFactors=F)
+    colnames(genome_coverage) = c('ldef', 'bp_covered')
+    genome_coverage$ldef = sapply(genome_coverage$ldef, function(l){gsub('_','.',l)})
+    genome_coverage$ldef = gsub('.ldef','',genome_coverage$ldef)
+    genome_coverage$prop_covered = genome_coverage$bp_covered / genome_size
+
 # Join num_peaks to num_uniq_peaks on the tf
 num_uniq_peaks$num_peaks = num_peaks[match(num_uniq_peaks$tf, num_peaks$tf), 'num_peaks']
 
 # Per experiment, what is the percentage of peaks caught?
 num_uniq_peaks$prop_caught = num_uniq_peaks$num_caught / num_uniq_peaks$num_peaks
 
+# Join genome coverage
+num_uniq_peaks$prop_coverage = genome_coverage[match(num_uniq_peaks$ldef, genome_coverage$ldef), 'prop_covered']
+
 # Overall peak catching per ldef
-peak_catching_ldef = summarize(group_by(num_uniq_peaks, ldef), mean_chipenrich_caught = mean(prop_caught), n = n())
+peak_catching_ldef = summarize(group_by(num_uniq_peaks, ldef), mean_chipenrich_caught = mean(prop_caught), prop_covered = mean(prop_coverage), n = n())
 
 # A tibble: 6 × 3
 #                          ldef mean_chipenrich_caught     n
@@ -38,13 +53,17 @@ peak_catching_ldef = summarize(group_by(num_uniq_peaks, ldef), mean_chipenrich_c
 # Compare to what Heming calculated based on findOverlaps().
 # NOTE: The difference is findOverlaps() considers any overlap whereas chipenrich requires the midpoint be in the locus
 combined = merge(peak_catching_ldef, tt_peak_catching, by.x='ldef', by.y='dataset')
-colnames(combined) = c('ldef','mean_chipenrich_caught','n_datasets','genome_coverage_heming','mean_findOverlap_caught','numEnh_ge1Gene','genome_coverage_tingting')
+colnames(combined) = c('ldef','mean_chipenrich_caught','genome_coverage_raymond','n_datasets','genome_coverage_heming','mean_findOverlap_caught','numEnh_ge1Gene','genome_coverage_tingting')
 
 # Write tabular output
 write.table(combined, file='peak_catching_compare_chipenrich_findoverlaps.txt', sep='\t', row.names=F, col.names=T, quote=F)
 
+caught_by_coverage_raymond = ggplot(data = combined, aes(x=mean_chipenrich_caught, y=genome_coverage_raymond, text = paste('ldef:', ldef))) +
+    geom_point(alpha=0.5) + xlab('Peaks Caught (by midpoint)') + ylab('Genome Coverage (Raymond)')
+ggsave(filename='midpoint_peak_catching_genome_coverage_raymond.pdf', plot = caught_by_coverage_raymond, width=6, height=6)
+
 caught_by_coverage = ggplot(data = combined, aes(x=mean_chipenrich_caught, y=genome_coverage_tingting, text = paste('ldef:', ldef))) +
-    geom_point(alpha=0.5) + xlab('Peaks Caught (by midpoint)') + ylab('Genome Coverage')
+    geom_point(alpha=0.5) + xlab('Peaks Caught (by midpoint)') + ylab('Genome Coverage (Ting Ting)')
 ggsave(filename='midpoint_peak_catching_genome_coverage.pdf', plot = caught_by_coverage, width=6, height=6)
 
 midpoint_by_findoverlaps = ggplot(data = combined, aes(x=mean_chipenrich_caught, y=mean_findOverlap_caught, text = paste('ldef:', ldef))) +
