92f1e011887510b654ecc4d923794b0f375799f2
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-06-20 14:42:25 -0400

    Bugfix: Calculate genome coverage over unique ldef regions
    * We were multi-counting regions assigned to multiple gene IDs, oops.

--- peak_catching/genome_coverage.sh
+++ peak_catching/genome_coverage.sh
@@ -9,3 +9,5 @@ for file in `ls ~/latte/share/improving_enhancer_definitions/locusdefs/*.ldef`
 do
 	echo `basename ${file}` `awk 'NR > 1 {SUM += $3 - $2} END {print SUM}' ${file}` >> genome_coverage.txt
 done
+
+echo `basename ${file}` `awk -v OFS='\t' 'NR > 1 {print $1, $2, $3}' ${file} | sort -T . -k1,1 -k2,2n | uniq | awk -v OFS='\t' '{SUM += $3 - $2} END {print SUM}'` >> genome_coverage.txt
