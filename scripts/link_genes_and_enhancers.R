0a225e6fd0d3f873804f4a205e5018d8f4b47daf
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-06-29 18:11:43 -0400

    Add QC plots to link_genes_and_enhancers.R
    * For point_to_point: a text file with the number of interactions used, how many interactions are gene/enh, gene/gene, and enh/enh; a plot of the distributions for the interaction distances for gene/enh, gene/gene, enh/enh and enh/nearest TSS.
    * For encompassing: a text file with the total number of loops, the number of valid loops (CTCF motif convergent), the mean number of loops per gene, genes per loop, enhancers per loop, loops per enhancer, genes per enhancer, and enhancers per gene; a plot of the distributions of the same.
    * Making these QC plots revealed that the fantom interactions were incorrectly interpreted! So good job QC plots.

--- scripts/link_genes_and_enhancers.R
+++ scripts/link_genes_and_enhancers.R
@@ -39,9 +39,40 @@ if (length(opts$max_genes_per_region) > 0 & opts$method == "point_to_point") {
 
 #####  LOAD THE REST OF THE LIBRARIES  #####
 suppressPackageStartupMessages(library("GenomicRanges"))
+suppressPackageStartupMessages(library("ggplot2"))
+suppressPackageStartupMessages(library("reshape2"))
 
 
 #####  MAIN  #####
+qc_prefix = gsub('.pairs', '', opts$out)
+p2p_qc_file = 'QC_P2P_interactions.txt'
+if(!file.exists(p2p_qc_file)) {
+    header = data.frame(
+        'pair_type' = 'pair_type',
+        'total_interactions' = 'total_interactions',
+        'gene_enh_pairs' = 'gene_enh_pairs',
+        'gene_gene_pairs' = 'gene_gene_pairs',
+        'enh_enh_pairs' = 'enh_enh_pairs', stringsAsFactors = F)
+    write.table(header, file = p2p_qc_file, sep = '\t', quote = F, col.names = F, row.names = F, append = T)
+}
+
+e_qc_file = 'QC_E_interactions.txt'
+if(!file.exists(e_qc_file)) {
+    header = data.frame(
+        'pair_type' = 'pair_type',
+        'total_loops' = 'total_loops',
+        'valid_loops' = 'valid_loops',
+        'loops_per_gene' = 'loops_per_gene',
+        'genes_per_loop' = 'genes_per_loop',
+        'loops_per_enh' = 'loops_per_enh',
+        'enhs_per_loop' = 'enhs_per_loop',
+        'enhs_per_gene' = 'enhs_per_gene',
+        'genes_per_enh' = 'genes_per_enh',
+        stringsAsFactors = F)
+    write.table(header, file = e_qc_file, sep = '\t', quote = F, col.names = F, row.names = F, append = T)
+}
+
+
 message(sprintf('Building %s...', opts$out))
 
 ###
@@ -84,6 +115,9 @@ if (opts$method == "point_to_point") {
   idx1 = merge(genes_interactions1, enhancers_interactions2, by = 'subjectHits', suffixes= c('.genes','.enh'))
   idx2 = merge(genes_interactions2, enhancers_interactions1, by = 'subjectHits', suffixes= c('.genes','.enh'))
 
+  gene_gene_idx = merge(genes_interactions1, genes_interactions2, by = 'subjectHits', suffixes = c('.1','.2'))
+  enh_enh_idx = merge(enhancers_interactions1, enhancers_interactions2, by = 'subjectHits', suffixes = c('.1','.2'))
+
   ###
   loci1_gr = enhancers_gr[idx1$queryHits.enh]
   loci1_gr$gene_id = genes_gr$gene_id[idx1$queryHits.genes]
@@ -110,6 +144,48 @@ if (opts$method == "point_to_point") {
 
   write.table(x = loci_df, file = opts$out, append = F, quote = F, sep = "\t", row.names = F, col.names = T)
 
+    #################################
+    # QC
+    qc_text = data.frame(
+        'pair_type' = qc_prefix,
+        'total_interactions' = nrow(interactions),
+        'gene_enh_pairs' = nrow(idx1) + nrow(idx2),
+        'gene_gene_pairs' = nrow(gene_gene_idx),
+        'enh_enh_pairs' = nrow(enh_enh_idx),
+        stringsAsFactors = F)
+    write.table(qc_text, file = p2p_qc_file, sep = '\t', quote = F, col.names = F, row.names = F, append = T)
+
+    # Distances
+    # Distance from gene to enhancers
+    unique_gene_enh_idx = unique(rbind(idx1[,2:3], idx2[,2:3]))
+
+    # Interaction distances for gene gene
+    unique_gene_gene_idx = unique(gene_gene_idx[,1])
+
+    # Interaction distances for enh enh
+    unique_enh_enh_idx = unique(enh_enh_idx[,1])
+
+    distance_list = list(
+        'gene_enh' = distance(genes_gr[unique_gene_enh_idx$queryHits.genes], enhancers_gr[unique_gene_enh_idx$queryHits.enh]),
+        'gene_gene' = distance(interactions1_gr[unique_gene_gene_idx], interactions2_gr[unique_gene_gene_idx]),
+        'enh_enh' = distance(interactions1_gr[unique_enh_enh_idx], interactions2_gr[unique_enh_enh_idx]),
+        'nearest' = mcols(distanceToNearest(enhancers_gr, genes_gr))$distance)
+    combined = reshape2::melt(distance_list)
+    colnames(combined) = c('distance', 'type')
+
+    nonzeros = which(combined$distance != 0)
+    combined$distance[nonzeros] = log10(combined$distance[nonzeros])
+
+    plot_distance = ggplot(data = combined, aes(x = distance)) +
+        geom_histogram(binwidth = 0.25) +
+        facet_grid(. ~ type) +
+        theme_bw() + ggtitle(sprintf('%s connection distances', qc_prefix)) + xlab('log10_distance')
+    ggsave(plot_distance, filename = sprintf('QC_%s_connection_distances.pdf', qc_prefix), height = 5, width = 20)
+
+    # Interaction widths
+    interaction_widths = width(c(interactions1_gr, interactions2_gr))
+    #################################
+
 } else if (opts$method == "encompassing") {
 
   message('Building E pairs...')
@@ -127,23 +203,29 @@ if (opts$method == "point_to_point") {
   ###
   # Each subjectHit index represents a loop. Use table to count how many genes fall in each loop
   num_genes_in_loops = table(genes_in_loops$subjectHits)
-
-  valid_loop_idx = names(num_genes_in_loops[num_genes_in_loops <= opts$max_genes_per_region])
-
-  ###
-  genes_in_loops = subset(genes_in_loops, subjectHits %in% valid_loop_idx)
-  enhancers_in_loops = subset(enhancers_in_loops, subjectHits %in% valid_loop_idx)
+  num_enh_in_loops = table(enhancers_in_loops$subjectHits)
 
   ###
+  valid_loop_idx = as.integer(names(num_genes_in_loops[num_genes_in_loops <= opts$max_genes_per_region]))
+  valid_genes_in_loops = subset(genes_in_loops, subjectHits %in% valid_loop_idx)
+  valid_enhancers_in_loops = subset(enhancers_in_loops, subjectHits %in% valid_loop_idx)
 
-  idx = merge(genes_in_loops, enhancers_in_loops, by = 'subjectHits', suffixes = c('.genes', '.enh'))
+  ###
+  idx = merge(valid_genes_in_loops, valid_enhancers_in_loops, by = 'subjectHits', suffixes = c('.genes', '.enh'))
 
   ###
   loci_gr = enhancers_gr[idx$queryHits.enh]
   loci_gr$gene_id = genes_gr$gene_id[idx$queryHits.genes]
 
+  # Split by gene_id before doing unique
+  loci_grl = split(loci_gr, loci_gr$gene_id)
+  loci_grl = unique(loci_grl)
+  loci_gr_geneid = S4Vectors::Rle(names(loci_grl), S4Vectors::elementNROWS(loci_grl))
+
+  loci_gr = unlist(loci_grl, use.names = FALSE)
+  loci_gr$gene_id = loci_gr_geneid
+
   loci_gr = sort(loci_gr)
-  loci_gr = unique(loci_gr)
 
   loci_df = as.data.frame(loci_gr)
   colnames(loci_df) = c('chr','start','end','width','strand','gene_id')
@@ -151,4 +233,56 @@ if (opts$method == "point_to_point") {
   loci_df = loci_df[,c('chr','start','end','gene_id')]
 
   write.table(x = loci_df, file = opts$out, append = F, quote = F, sep = "\t", row.names = F, col.names = T)
+
+    #################################
+    # QC
+    num_total_loops = length(loops_gr)
+    num_valid_loops = length(valid_loop_idx)
+
+    unique_loop_gene = unique(idx[,c('subjectHits','queryHits.genes')])
+    unique_loop_enh = unique(idx[,c('subjectHits','queryHits.enh')])
+    unique_gene_enh = unique(idx[,c('queryHits.genes','queryHits.enh')])
+
+    # Pers
+    per_list = list(
+        'loops_per_gene' = table(unique_loop_gene$queryHits.genes),
+        'genes_per_loop' = table(unique_loop_gene$subjectHits),
+        'loops_per_enh' = table(unique_loop_enh$queryHits.enh),
+        'enhs_per_loop' = table(unique_loop_enh$subjectHits),
+        'enhs_per_gene' = table(unique_gene_enh$queryHits.genes),
+        'genes_per_enh' = table(unique_gene_enh$queryHits.enh))
+    per = reshape2::melt(lapply(per_list, as.integer))
+    colnames(per) = c('per', 'type')
+
+    pdf(file = sprintf('QC_%s.pdf', qc_prefix), width = 12, height = 18)
+    par(mfrow = c(3,2))
+    for(t in unique(per$type)) {
+        hist(subset(per, type == t)$per, xlab = t, main = sprintf('%s %s', qc_prefix, t))
+    }
+    dev.off()
+
+    if(!file.exists(sprintf('QC_loop_widths.pdf', qc_prefix))) {
+        # Loop widths
+        loop_widths_list = list(
+            'all' = log10(width(loops_gr)),
+            'valid' = log10(width(loops_gr[valid_loop_idx])))
+        loop_widths = reshape2::melt(loop_widths_list)
+        colnames(loop_widths) = c('log10_width', 'type')
+
+        plot_widths = ggplot(data = loop_widths, aes(x = log10_width)) +
+            geom_histogram(binwidth = 0.25) +
+            facet_grid(. ~ type) +
+            theme_bw() + ggtitle(sprintf('Loop widths', qc_prefix))
+        ggsave(plot_widths, filename = sprintf('QC_loop_widths.pdf', qc_prefix), height = 6, width = 8)
+    }
+
+    # Text
+    qc_text = data.frame(
+        'pair_type' = qc_prefix,
+        'total_loops' = num_total_loops,
+        'valid_loops' = num_valid_loops,
+        t(sapply(per_list, function(p){mean(as.integer(p))})),
+        stringsAsFactors = F)
+    write.table(qc_text, file = e_qc_file, sep = '\t', quote = F, col.names = F, row.names = F, append = T)
+    #################################
 }
