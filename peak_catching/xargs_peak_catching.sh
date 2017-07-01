6c897159660a1e2310449190fcabe1767117e662
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-07-01 12:06:27 -0400

    Bugfix: Command substitution mistake

--- peak_catching/xargs_peak_catching.sh
+++ peak_catching/xargs_peak_catching.sh
@@ -15,6 +15,6 @@ outfile=${basepeak}_${baseldef}.counts
 # The pipe is to grab only the unique peaks (cols 1-3 should be ordered)
 NUM_CAUGHT=`bedtools intersect -wa -wb \
     -a <(awk -v OFS='\t' '{print $1, int(($3 + $2)/2), int(($3 + $2)/2)}' ${peak} | sort -T . -k1,1 -k2,2n) \
-    -b <(gunzip -c ${ldef}) | awk -v OFS='\t' 'NR > 1 {print $0}') \
+    -b <(gunzip -c ${ldef} | awk -v OFS='\t' 'NR > 1 {print $0}') \
 | cut -f 1-3 | uniq | wc -l`
 echo ${basepeak} ${baseldef} ${NUM_CAUGHT} > ${outfile}
