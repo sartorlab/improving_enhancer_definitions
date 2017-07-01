fc93126d56688c7ce4a083c222ebbeb54e489101
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-07-01 11:38:10 -0400

    Bugfix: Wrong function to gzip chipenrich ldefs
    * Oops

--- peak_catching/extract_chipenrich_locusdefs.R
+++ peak_catching/extract_chipenrich_locusdefs.R
@@ -8,6 +8,6 @@ fivekb_df = locusdef.hg19.5kb@dframe
 fivekb_outside_df = locusdef.hg19.5kb_outside@dframe
 ntss_df = locusdef.hg19.nearest_tss@dframe
 
-write.table(fivekb_df, file = gzip('5kb.ldef'), sep='\t', col.names = T, row.names = F, quote = F
-write.table(fivekb_outside_df, file = gzip('5kb_outside.ldef'), sep='\t', col.names = T, row.names = F, quote = F
-write.table(ntss_df, file = gzip('nearest_tss.ldef'), sep='\t', col.names = T, row.names = F, quote = F)
+write.table(fivekb_df, file = gzfile('5kb.ldef'), sep='\t', col.names = T, row.names = F, quote = F
+write.table(fivekb_outside_df, file = gzfile('5kb_outside.ldef'), sep='\t', col.names = T, row.names = F, quote = F
+write.table(ntss_df, file = gzfile('nearest_tss.ldef'), sep='\t', col.names = T, row.names = F, quote = F)
